2cb0014bc07f945364053f79020de85b48b6a69d
Carla <carlajpont@gmail.com>
2023-02-08 12:04:50 +0000

    Changing styling

--- src/components/sidebar/BulletinParagraph.tsx
+++ src/components/sidebar/BulletinParagraph.tsx
@@ -1,25 +1,27 @@
 import React, { memo } from "react";
-import { SubHeader } from "../../styles/typography.styles";
+import { Header3 } from "../../styles/typography.style";
+import { Container } from "../../styles/sidebar.style";
 
 type SummaryProps = {
   title?: string;
   content: string;
+  marginTop?: string;
 };
 
-const BulletinParagraph: React.FC<SummaryProps> = ({ title, content }) => {
+const BulletinParagraph: React.FC<SummaryProps> = ({
+  title,
+  content,
+  marginTop,
+}) => {
   if (!content) {
     return null;
   }
+
   return (
-    <div>
-      {title ? (
-        <SubHeader style={{ marginBottom: 0 }}>{title}</SubHeader>
-      ) : null}
-      <p
-        style={{ fontSize: "15px" }}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
-    </div>
+    <Container>
+      {title ? <Header3 marginTop={marginTop}>{title}</Header3> : null}
+      <p dangerouslySetInnerHTML={{ __html: content }} />
+    </Container>
   );
 };
 export default memo(BulletinParagraph);
