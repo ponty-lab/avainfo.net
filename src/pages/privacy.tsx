47ebce20f1165655b6a9595833b1ebfbae2420e4
Carla <carlajpont@gmail.com>
2023-03-02 22:28:34 +0000

    Content for privacy policy

---
+++ src/pages/privacy.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { Page } from "../styles/pages.style";
+import privacy from "./privacy.md";
+import rehypeRaw from "rehype-raw";
+
+const Docs = () => {
+  const [markdown, setMarkdown] = useState("");
+
+  useEffect(() => {
+    fetch(privacy)
+      .then((res) => res.text())
+      .then((text) => setMarkdown(text));
+  }, []);
+
+  return (
+    <Page>
+      <ReactMarkdown
+        children={markdown}
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
+      />
+    </Page>
+  );
+};
+
+export default Docs;
