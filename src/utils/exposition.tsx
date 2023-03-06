426917bd8d353fca60bdde8072b0919d4d8b6299
Carla <carlajpont@gmail.com>
2023-03-06 20:08:05 +0000

    Using ES6 imports

--- src/utils/exposition.tsx
+++ src/utils/exposition.tsx
@@ -1,18 +1,30 @@
+import {
+  expoBG,
+  expoN,
+  expoNE,
+  expoE,
+  expoSE,
+  expoS,
+  expoSW,
+  expoW,
+  expoNW,
+} from "../assets/expositions";
+
 class ExpositionIcon {
   uri;
-  constructor(uri: any) {
+  constructor(uri: string) {
     this.uri = uri;
   }
 }
 
 export const Exposition = {
-  N: new ExpositionIcon(require("../assets/expositions/exposition_n.png")),
-  NE: new ExpositionIcon(require("../assets/expositions/exposition_ne.png")),
-  E: new ExpositionIcon(require("../assets/expositions/exposition_e.png")),
-  SE: new ExpositionIcon(require("../assets/expositions/exposition_se.png")),
-  S: new ExpositionIcon(require("../assets/expositions/exposition_s.png")),
-  SW: new ExpositionIcon(require("../assets/expositions/exposition_sw.png")),
-  W: new ExpositionIcon(require("../assets/expositions/exposition_w.png")),
-  NW: new ExpositionIcon(require("../assets/expositions/exposition_nw.png")),
-  BG: new ExpositionIcon(require("../assets/expositions/exposition_bg.png")),
+  N: new ExpositionIcon(expoN),
+  NE: new ExpositionIcon(expoNE),
+  E: new ExpositionIcon(expoE),
+  SE: new ExpositionIcon(expoSE),
+  S: new ExpositionIcon(expoS),
+  SW: new ExpositionIcon(expoSW),
+  W: new ExpositionIcon(expoW),
+  NW: new ExpositionIcon(expoNW),
+  BG: new ExpositionIcon(expoBG),
 };
