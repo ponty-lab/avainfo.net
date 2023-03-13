7514cc33148893e94d60e55ab1ba0d0046f9837c
Carla <carlajpont@gmail.com>
2023-03-13 19:31:32 +0000

    Applying device constants to media queries

--- src/pages/about/AboutAppleBadge.tsx
+++ src/pages/about/AboutAppleBadge.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import AppleBadge from "../../assets/badges/apple-badge.svg";
+import { device } from "../../utils/constants";
 
 const APPLE_STORE_URL =
   "https://apps.apple.com/us/app/avainfo/id1552158809?itsct=apps_box_badge&amp;itscg=30200";
@@ -16,25 +17,24 @@ const AppleLink = styled.a`
   display: inline-block;
   overflow: hidden;
   border-radius: 13px;
-  width: 150px;
+  margin-left: 10px;
+  width: 170px;
   height: auto;
 
-  @media screen and (max-width: 360px) {
-    margin-left: 10px;
-    width: 170px;
-    height: auto;
+  @media screen and (${device.mobileM}) {
+    margin-left: 0px;
+    width: 150px;
   }
 `;
 
 const AppleImg = styled.img`
   border-radius: 13px;
-  width: 155px;
+  width: 170px;
   height: auto;
   margin: 12px 0px;
 
-  @media screen and (max-width: 360px) {
-    width: 170px;
-    height: auto;
+  @media screen and (${device.mobileM}) {
+    width: 155px;
   }
 `;
 
