bb152000a598f71249a6f7ca550f3bc72e85853c
Carla <carlajpont@gmail.com>
2023-03-15 18:46:08 +0000

    Increasing transparency on no_rating danger color

--- src/styles/colors.style.tsx
+++ src/styles/colors.style.tsx
@@ -1,8 +1,10 @@
+import { hexToRGB } from "../utils/hexToRGB";
+
+const lightBlue = hexToRGB("#286882", "0.4");
+
 const AvaColors: Record<string, string> = {
-  "/": "#42799a",
-  "n/a": "#42799a",
-  "-1": "#42799a",
-  "0": "#42799a",
+  "-1": lightBlue,
+  "0": lightBlue,
   "1": "#ccff66",
   "2": "#ffff00",
   "3": "#ff9900",
