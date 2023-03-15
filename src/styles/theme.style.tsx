764c89dbc2b61a51250e748e5c08bb1e854aa30d
Carla <carlajpont@gmail.com>
2023-03-15 18:38:23 +0000

    Removing unused styles

--- src/styles/theme.style.tsx
+++ src/styles/theme.style.tsx
@@ -8,26 +8,14 @@ declare module "styled-components" {
       background: string;
       text: string;
     };
-    dangerColors: {};
   }
 }
 
 export const lightTheme: DefaultTheme = {
   colors: {
-    //AvaInfo theme colors
     primary: "#286882",
     accent: "#F18D39",
     background: "#ffffff",
     text: "#0e2127",
-  },
-  dangerColors: {
-    "n/a": "#42799a",
-    "-1": "#42799a",
-    0: "#42799a",
-    1: "#ccff66",
-    2: "#ffff00",
-    3: "#ff9900",
-    4: "#ff0000",
-    5: "#600000",
   },
 };
