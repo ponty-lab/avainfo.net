5ec66c02d8fc14411090952becc3ef98b666b14e
Carla <carlajpont@gmail.com>
2023-03-27 22:22:13 +0100

    Mapbox polygons and region names

---
+++ functions/assets/index.ts
@@ -0,0 +1,60 @@
+import { ADGeoJson } from "./mapbox/AD_micro-regions.geojson";
+import { AT02GeoJson } from "./mapbox/AT-02_micro-regions.geojson";
+import { AT03GeoJson } from "./mapbox/AT-03_micro-regions.geojson";
+import { AT04GeoJson } from "./mapbox/AT-04_micro-regions.geojson";
+import { AT05GeoJson } from "./mapbox/AT-05_micro-regions.geojson";
+import { AT06GeoJson } from "./mapbox/AT-06_micro-regions.geojson";
+import { AT07GeoJson } from "./mapbox/AT-07_micro-regions.geojson";
+import { AT08GeoJson } from "./mapbox/AT-08_micro-regions.geojson";
+import { CHGeoJson } from "./mapbox/CH_regions.geojson";
+import { DEBYGeoJson } from "./mapbox/DE-BY_micro-regions.geojson";
+import { ESCTLGeoJson } from "./mapbox/ES-CT-L_micro-regions.geojson";
+import { FRGeoJson } from "./mapbox/FR_regions.geojson";
+import { IT21GeoJson } from "./mapbox/IT-21_micro-regions.geojson";
+import { IT23GeoJson } from "./mapbox/IT-23_micro-regions.geojson";
+import { IT25GeoJson } from "./mapbox/IT-25_micro-regions.geojson";
+import { IT32BZGeoJson } from "./mapbox/IT-32-BZ_micro-regions.geojson";
+import { IT32TNGeoJson } from "./mapbox/IT-32-TN_micro-regions.geojson";
+import { IT34GeoJson } from "./mapbox/IT-34_micro-regions.geojson";
+import { IT36GeoJson } from "./mapbox/IT-36_micro-regions.geojson";
+// import { IT42GeoJson } from "./mapbox/IT-42_outline.geojson";
+// import { IT45GeoJson } from "./mapbox/IT-45_outline.geojson";
+// import { IT52GeoJson } from "./mapbox/IT-52_outline.geojson";
+import { IT57GeoJson } from "./mapbox/IT-57_micro-regions.geojson";
+// import { IT65GeoJson } from "./mapbox/IT-65_outline.geojson";
+// import { IT77GeoJson } from "./mapbox/IT-77_outline.geojson";
+// import { IT82GeoJson } from "./mapbox/IT-82_outline.geojson";
+import { SIGeoJson } from "./mapbox/SI_micro-regions.geojson";
+
+export { FRGeoJson } from "./mapbox/FR_regions.geojson";
+export { regionNames } from "./region_names_en";
+
+export const EuropeShapefileSource = [
+  ADGeoJson,
+  AT02GeoJson,
+  AT03GeoJson,
+  AT04GeoJson,
+  AT05GeoJson,
+  AT06GeoJson,
+  AT07GeoJson,
+  AT08GeoJson,
+  CHGeoJson,
+  DEBYGeoJson,
+  ESCTLGeoJson,
+  FRGeoJson,
+  IT21GeoJson,
+  IT23GeoJson,
+  IT25GeoJson,
+  IT32BZGeoJson,
+  IT32TNGeoJson,
+  IT34GeoJson,
+  IT36GeoJson,
+  // IT42GeoJson,
+  // IT45GeoJson,
+  // IT52GeoJson,
+  IT57GeoJson,
+  // IT65GeoJson,
+  // IT77GeoJson,
+  // IT82GeoJson,
+  SIGeoJson,
+];
